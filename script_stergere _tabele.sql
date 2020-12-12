de106531804958596ea457fc3610bff5e3d5a8de
CosminCCC29 <cosminccc28@gmail.com>
2020-12-12 22:51:05 +0200

    Minor tweaks

--- script_stergere _tabele.sql
+++ script_stergere _tabele.sql
@@ -1,10 +1,12 @@
 -- Stergere tabele
 
--- nume_prenume REGEXP_LIKE(nume_atribut, '^[A-Za-z]+\ (\-?[A-Za-z]+)+$')
--- parola REGEXP_LIKE(nume_atribut, '^\S+$')
--- nume_tip REGEXP_LIKE(nume_atribut, '^[A-Za-z]+\-?[A-Za-z]+$')
--- nume_categorie REGEXP_LIKE(nume_atribut, '^[A-Za-z]+\ ?(\ ?[A-Za-z]+)+$')
--- nume_produs REGEXP_LIKE(nume_atribut, '^[A-Za-z]+(\ ?[A-Za-z[0-9]+(\.[0-9]+)?)+$')
+-- nume_prenume     REGEXP_LIKE(nume_prenume_cont, '^([A-Z][a-z]+)\ ([A-Z][a-z]+)(\-[A-Z][a-z]+)?$) --
+-- parola           REGEXP_LIKE(parola_cont, '^\S+$')
+-- nume_tip         REGEXP_LIKE(nume_tip, '^[a-z]+\-?[a-z]+$')
+-- nume_categorie   REGEXP_LIKE(nume_categorie, '^[A-Z][a-z]+(\ ?[a-z]+)*$') --
+-- nume_ingredient  REGEXP_LIKE(nume_ingredient, '^[a-z]+(\ ?[a-z]+)*$') --
+-- nume_meniu       REGEXP_LIKE(nume_meniu, '^[A-Z][a-z]+([\ \-]?[A-Z]?[a-z0-9]+)*$') --
+-- nume_produs      REGEXP_LIKE(nume_produs, '^[A-Z][a-z]+(\ ?[A-Za-z0-9]+(\.[0-9]+)?)*$') --
 
 BEGIN
 
